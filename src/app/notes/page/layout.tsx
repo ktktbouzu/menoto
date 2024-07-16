63d90a5ea2255a3fe7ae478e7babebdfc78f24c9
Aldrin Labarda <aldrinthomaslab@gmail.com>
2024-07-16 20:47:48 +0800

    Transferred grid layout page to note page

--- src/app/notes/page/layout.tsx
+++ src/app/notes/page/layout.tsx
@@ -1,10 +1,12 @@
 import { Grid } from '@mui/material';
 
-import Sidebar from '../components/sidebar';
+import Sidebar from '../../components/sidebar';
+
+import '../../styles/layouts/notePage.scss';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Grid container spacing={0}>
+    <Grid container spacing={0} className="note-page">
       <Grid item xs={2}>
         <Sidebar />
       </Grid>
