0f2c172d6d105ad5c1f33696218f96ba6e09f71e
Aldrin Labarda <aldrinthomaslab@gmail.com>
2024-07-16 20:48:36 +0800

    Removed milkdown provider component

--- src/app/notes/page/page.tsx
+++ src/app/notes/page/page.tsx
@@ -1,14 +1,7 @@
 'use client';
-import { MilkdownProvider } from '@milkdown/react';
 
 import NoteEditor from "@/app/components/noteEditor";
 
 export default function Page() {
-  return (
-    <div>
-      <MilkdownProvider>
-        <NoteEditor />
-      </MilkdownProvider>
-    </div>
-  );
+  return <NoteEditor />;
 }
