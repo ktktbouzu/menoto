725a5d45a278e4a30662f362a44426e9c42fa0b7
Aldrin Labarda <aldrinthomaslab@gmail.com>
2024-07-16 20:27:40 +0800

    Added styles to sidebar component

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,5 +1,17 @@
+import '../styles/components/sidebar.scss';
+
 export default function Sidebar() {
   return (<div>
-    <h4>Notes</h4>
+    <div className="sidebar__title-bar">
+      <h3>My Notes</h3>
+    </div>
+    <ul className="sidebar__list">
+      <li>Note 1</li>
+      <li>Note 2</li>
+      <li>Note 3</li>
+      <li>Note 4</li>
+      <li>Note 5</li>
+      <li>Note 6</li>
+    </ul>
   </div>)
 }
