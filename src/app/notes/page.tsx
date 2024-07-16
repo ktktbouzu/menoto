0729373da0e215d83b26abde57b3bbe8ab1a8d11
Aldrin Labarda <aldrinthomaslab@gmail.com>
2024-07-16 21:56:12 +0800

    Added styles to note page elements

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,24 @@
+import Link from 'next/link'
+import { Container, Box } from '@mui/material';
+
+import '../styles/pages/noteList.scss';
+
 export default function Page() {
-  return <p>Notes list page.</p>
+  const items = [...Array(10).keys()];
+
+  return (
+    <Container className="note-list" maxWidth="md">
+      <h2 className="note-list__header">My Notes</h2>
+      <Box className="note-list__content">
+        { items.map( number => (
+            <Box className={`note-list__item note-list__item--${number}`}>
+              <Link href="/notes/page">
+                <h4>Note {number + 1}</h4>
+              </Link>
+            </Box>
+          )) 
+        }
+      </Box>
+    </Container>
+  );
 }
