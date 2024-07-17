93018dfe47adb2fbc2220f3755ddf3d1bf215495
Aldrin Labarda <aldrinthomaslab@gmail.com>
2024-07-17 13:18:28 +0800

    Added style to note editor view

--- src/app/notes/page/page.tsx
+++ src/app/notes/page/page.tsx
@@ -1,7 +1,17 @@
 'use client';
+import { Box } from '@mui/material';
 
 import NoteEditor from "@/app/components/noteEditor";
 
+import '../../styles/pages/noteView.scss';
+
 export default function Page() {
-  return <NoteEditor />;
+  return (
+    <Box className="note-view">
+      <Box className="note-view__header">
+        <h3>Note Title</h3>
+      </Box>
+      <NoteEditor />
+    </Box>
+  );
 }
